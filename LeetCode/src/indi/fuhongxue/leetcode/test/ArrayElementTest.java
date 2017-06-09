6b7feb3bd489dd73ca626377672094117974f71b
jfdw <j.fu@outlook.com>
2017-06-09 23:33:45 +0800

    测试类更改为TestNG

--- LeetCode/src/indi/fuhongxue/leetcode/test/ArrayElementTest.java
+++ LeetCode/src/indi/fuhongxue/leetcode/test/ArrayElementTest.java
@@ -1,52 +1,42 @@
 package indi.fuhongxue.leetcode.test;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import indi.fuhongxue.leetcode.arrays.ArrayElement;
 import indi.fuhongxue.leetcode.arrays.ArrayUtils;
 
-//@RunWith(Parameterized.class)
 public class ArrayElementTest 
 {
-//	private int[] array;
-//	private int element;
-//	
-//	
-//	public ArrayElementTest(int[] array, int element)
-//	{
-//		this.array = array;
-//		this.element = element;
-//	}
-//
-//	@Parameters
-//	public static int[][][] arrays()
-//	{
-//		return new int[][][]{{{1,2,2,2,2,2,4,3,3,4,6},{2}},
-//			{{1,2,3,4,5},{6}}};
-//	}
+
+	@DataProvider
+	public int[][][] remove()
+	{
+		return new int[][][]{
+			{{1,2,2,2,2,2,4,3,3,4,6},//数组
+			{2},					 //待删除元素
+			{6}						 //期待数组长度
+			},
+			{{1,2,3,4,5},{5},{4}}};
+	}
 	
-	@Test
-	public void testRemoveElement() {
-//		fail("Not yet implemented");
-//		int[] array = new int[]{1,2,2,2,2,2,4,3,3,4,6};
-		int[] array = new int[]{1,2,3,4,5,6};
-		int length = ArrayElement.removeElement(array,8);
+	@Test(dataProvider="remove")
+	public void testRemoveElement(int[] array, int[] element,int[] expectedLength) {
+		int length = ArrayElement.removeElement(array,element[0]);
 		
 		//验证长度
-		Assert.assertEquals(length,6);
+		AssertJUnit.assertEquals(length,expectedLength[0]);
 		//打印结果
 		ArrayUtils.printArray(array, length);
 	}
 	
-	@Test
-	public void testRemove()
+	@Test(dataProvider="remove")
+	public void testRemove(int[] array, int[] element,int[] expectedLength)
 	{
-//		int[] array = new int[]{1,2,2,2,2,2,4,3,3,4,6};
-		int[] array = new int[]{1,2,3,4,5,6,1};
-		int length = ArrayElement.remove(array,1);
+		int length = ArrayElement.remove(array,element[0]);
 		
 		//验证长度
-		Assert.assertEquals(length,5);
+		AssertJUnit.assertEquals(length,expectedLength[0]);
 		//打印结果
 		ArrayUtils.printArray(array, length);
 	}
